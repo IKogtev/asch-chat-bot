42e5622417d6466040daa7edc42c56038b68d62f
iasivkov <i.a.sivkov@yandex.ru>
2026-04-08 16:32:35 +0300

    clear web ag-ui

--- web/copilot-web/next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  transpilePackages: ["@copilotkit/react-core", "@copilotkit/react-ui", "@copilotkit/runtime"],
-};
-
-export default nextConfig;
